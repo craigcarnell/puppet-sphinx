e8060ac1154a39a14ea7594f16e9a1dfbf4b5a18
Craig Carnell <craig_carnell@hotmail.com>
2017-05-15 09:44:08 +0100

    Make the config support more than one instance, WIP

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,9 +9,12 @@ class sphinx(
   $listen_ip = undef,
   $listen_port = undef,
   $config_file = undef,
-  $indexer_mem_limit = '32M',
-  $base_path = '/var/lib/sphinx',
-  $binlog_path = '/var/lib/sphinx'
+  $base_path = '/var/lib/sphinx_9312',
+  $binlog_path = '/var/lib/sphinx_9312',
+  $port = $sphinx::params::port,
+  $port_mysql41 = $sphinx::params::port_mysql41,
+  $indexer_mem_limit = $sphinx::params::indexer_mem_limit,
+
 ) inherits sphinx::params {
 
   contain sphinx::install
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,4 +2,9 @@ class sphinx::params() {
   $centos_package = "sphinx-2.2.11-1.rhel7.x86_64.rpm"
   $ubuntu_ppa = "ppa:builds/sphinxsearch-rel22"
   $sphinx_config_file = "/etc/sphinx/sphinx.conf"
+
+  $port = 9312
+  $port_mysql41 = 9306
+
+  $indexer_mem_limit	= "128M"
 }
